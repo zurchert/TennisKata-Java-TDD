5cfc4c8e4c4e9b3ff756d32c8f1f9148cc2cd61f
klimczam <mathis.klimczak@iut-valence.fr>
2015-10-20 13:20:07 +0200

    Second commit for 15-0

--- src/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGame.java
+++ src/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGame.java
@@ -2,9 +2,20 @@ package fr.iutvalence.info.dut.m3105.tenniskata.tdd;
 public class TennisGame
 {
 
+	String score;
+	
+	public TennisGame(){
+		this.score = "love-all";
+	}
+	
 	public String getScore() {
 		
-		return "love-all";
+		return this.score;
+	}
+
+	public void scoreServerIncrease() {
+		this.score = "fifteen-love";
+		
 	}
 	
 }
